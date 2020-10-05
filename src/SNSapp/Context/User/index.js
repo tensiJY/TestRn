ea9a62dd41a413a6a30658229121b6e4ff06e8dd
tensiJY <wleowndud05@naver.com>
2020-10-05 22:12:38 +0900

    22:12

--- src/SNSapp/Context/User/index.js
+++ src/SNSapp/Context/User/index.js
@@ -18,6 +18,20 @@ const UserContextProVider = ({children}) => {
     const [isLoding, setIsLoding] = useState(false);
 
     const login = (email, password)=>{
+        AsyncStorage.setItem('token', 'save your token')
+        .then( ()=>{
+            setUserInfo({
+                name : 'dev-yakuza',
+                email : 'dev.yakuza@gamil.com'
+            });
+            setIsLoding(true)
+        });
+    }// function login end
+
+
+    const getUserInfo = ()=>{
         
     }
+
+    useEffect(()=>{},[])
 }
