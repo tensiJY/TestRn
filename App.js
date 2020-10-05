e9fc743d54bbfa8265c7fb5984b9f698ec87ac2d
wndud <wleowndud05@naver.com>
2020-10-05 16:35:58 +0900

    kk

--- App.js
+++ App.js
@@ -2,9 +2,11 @@ import React from 'react';
 import {SafeAreaView, Text} from 'react-native';
 
 const App = ()=>{
+
     return(
         <SafeAreaView>
             <Text>1</Text>
         </SafeAreaView>
     )
+    
 }
