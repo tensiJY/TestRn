2b27136c94f7461436fcbbdd875d5a37da6764fc
wndud <wleowndud05@naver.com>
2020-10-06 14:32:59 +0900

    14:32

--- App.js
+++ App.js
@@ -1,13 +1,27 @@
-import AsyncStorage from '@react-native-community/async-storage';
-import React, { useEffect } from 'react';
-import {SafeAreaView, Text, View} from 'react-native';
+import React, { useContext, useEffect, useState } from 'react';
+
+import {SafeAreaView, Text, View, TouchableOpacity, TouchableHighlight, Image } from 'react-native';
 
 //  AsyncStorage Test
 import UserData from './src/Test/AsyncStorage'
-
+import {UserContext, UserContextProvider} from './src/SNSapp/Context/User'
 
 
 //import SNSapp from './src/SNSapp/SNSapp';
+
+const Cview = ()=>{
+
+    const {userInfo, isLoding} = useContext(UserContext);
+
+    console.log('Cview : ', userInfo);
+    console.log('Cview : ', isLoding)
+
+    return(
+        <View>
+            <Text>1</Text>
+        </View>
+    )
+}
 
 const App = ()=>{
     useEffect( ()=>{
@@ -15,13 +29,41 @@ const App = ()=>{
         
     }, [])
 
+    const textEvt1 = ()=>{
+        console.log(1)
+    }
+    const textEvt2 = ()=>{
+        console.log(2)
+    }
 
+    // return(
+    //     <SafeAreaView style={{flex:1, borderColor:'red', borderWidth : 1, }}>
+    //         <View style={{flex:1, flexDirection:'row', borderWidth: 1, borderColor:'blue'}}>
+    //             <View style={{flex:1,alignItems:'center',justifyContent:'center',flexDirection:'column'}}>
+    //                 <TouchableOpacity onPress={textEvt1} >
+    //                     <Image source={require('./src/SNSapp/Assets/Images/app_icon.png')} style={{width:50, height:50}} />
+    //                 </TouchableOpacity>
+    //             </View>
+
+    //             <View style={{flex:1}}>
+    //                 <TouchableOpacity onPress={textEvt2}>
+    //                     <Text>2</Text>
+    //                 </TouchableOpacity>
+    //             </View>
+    //         </View>
+
+    //         <View style={{flex:1}}>
+    //             <UserData />
+    //         </View>
+            
+    //     </SafeAreaView>
+    // )
+        
     return(
-        <View>
-            <UserData />
-        </View>
+        <UserContextProvider>
+           <Cview />
+        </UserContextProvider>
     )
-  
 
     // return(
     //     <SafeAreaView>
--- src/SNSapp/Context/RandomUserData/index.js
+++ src/SNSapp/Context/RandomUserData/index.js
@@ -1,1 +1,18 @@
-import React from 'react';
+import React, {createContext, useState, useEffect} from 'react';
+import {Image} from 'react-native';
+
+import AsyncStorage from '@react-native-community/async-storage';
+import Loding from '../../Components/Loding';
+
+const RandomUserDataContext = createContext({
+    //  number의 인수가 없을 경우 10
+    getMyFeed:(number=10)=>{
+        return [];
+    }
+});
+
+const RandomUserDataProvider = ({cache, children}) =>{
+
+}
+
+export {RandomUserDataContext, RandomUserDataProvider}
