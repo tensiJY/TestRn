e9a4017284fc0ff2dcbe2391709217a08a14aded
wndud <wleowndud05@naver.com>
2020-10-06 11:51:23 +0900

    11:51

---
+++ src/Test/AsyncStorage/index.js
@@ -0,0 +1,68 @@
+import React, {useState, useEffect} from 'react';
+import {StyleSheet, View, Text, TouchableOpacity, Button} from 'react-native';
+import AsyncStorage from '@react-native-community/async-storage';
+
+
+const userData = ( ) => {
+    const [userInfo, setUserInfo] = useState(undefined);
+
+    const getUserInfo = async () => {
+        try{
+            console.log('start getUserInfo')
+            await AsyncStorage.getItem('token')
+            .then( (value)=>{
+                if(value== undefined || value == null){
+                   console.log('getUserInfo : undefined')
+    
+                }else{
+                    console.log('getUserInfo : ', value)
+                }
+    
+            })
+            .catch( (error)=>{
+                console.log(error)
+            });//AsyncStorage.getItem
+        }catch(error){
+
+        }
+    }// getUserInfo end
+
+    const removeInfo = () => {
+        console.log('removeInfo')
+        AsyncStorage.removeItem('token');
+        
+    }
+
+    const setUserData = ()=> {
+        AsyncStorage.setItem('token', 'your token')
+        .then( ()=>{
+            
+            const userInfo = {
+                userId : 'park',
+                userPwd : '1234'
+            }
+
+            setUserInfo(userInfo);
+            console.log('userInfo : ', userInfo)
+        } )
+        .catch( (error)=>{
+            console.log(error)
+        })
+    }
+
+    useEffect( ()=>{
+        
+        getUserInfo();
+
+    },[])
+
+    return(
+        <TouchableOpacity >
+            <Button onPress={ removeInfo} title='삭제'/>
+            <View style={{marginTop : 16}}></View>
+            <Button onPress={setUserData} title='저장'/>
+        </TouchableOpacity>
+    )
+}
+
+export default userData;
