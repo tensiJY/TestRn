e75121acf3d1a1a8680bbcdcd3d9bd9e0bac5f1a
tensiJY <wleowndud05@naver.com>
2020-10-11 15:17:58 +0900

    2020-10-11 15:17

--- App.js
+++ App.js
@@ -1,39 +1,16 @@
 import React, { useContext, useEffect, useState } from 'react';
 import {SafeAreaView, Text, View, TouchableOpacity, TouchableHighlight, Image } from 'react-native';
 
-import DrawerTest from './src/Test/Drawer/Test1'
-
-//  AsyncStorage Test
-//import UserData from './src/Test/AsyncStorage'
-
-
-// import {UserContext, UserContextProvider} from './src/SNSapp/Context/User';
-// import {RandomUserDataContext, RandomUserDataProvider} from './src/SNSapp/Context/RandomUserData';
-// import Login from './src/SNSapp/Screens/Login'
-
-//import SNSapp from './src/SNSapp/SNSapp';
+//import DrawerTest from './src/Test/Drawer/Test1'
 
 
 
 const App = ()=>{
     return(
-        
-            <DrawerTest />
-            
+        <View>
+            <Text>1</Text>
+        </View>      
     )
-
-    // return(
-    //     <View style={{flex:1}}>
-    //         <Login/>
-    //     </View>
-    // )
-        
-    // return(
-    //     <SafeAreaView>
-    //         <SNSapp />
-            
-    //     </SafeAreaView>
-    // )
 
 }
 
