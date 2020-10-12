8daff93e423fdaf16eb69eeb046477a490c2f4d8
tensiJY <wleowndud05@naver.com>
2020-10-12 18:38:30 +0900

    ''

--- src/SNSapp/Screens/MyFeed/index.js
+++ src/SNSapp/Screens/MyFeed/index.js
@@ -1,4 +1,59 @@
-import React from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import {FlatList, StyleSheet, View} from 'react-native';
+import {RandomUserDataContext} from '../../Context/RandomUserData'
+import IconButton from '../../Components/IconButton';
+import Feed from '../../Components/Feed';
 
+import StoryList from './StoryList';
 
+const MyFeed = ({navigation}) =>{
+    const {getMyFeed} = useContext(RandomUserDataContext);
+
+    const [feedList, setFeedList] = useState([]);
+    const [storyList, setStoryList] = useState([]);
+    const [loding, setLoding] = useState(false);
+   
+    React.useLayoutEffect( ()=>{
+        navigation.setOptions({
+            headerLeft : () => <IconButton iconName="camera" />,
+            headerRight : () => (
+                <View style={styles.headerRightcontaienr}>
+                    <IconButton iconName='live' />
+                    <IconButton iconName='send' />
+                </View>
+            ),
+        });
+    },[] )
+
+    useEffect( ()=> {
+        setFeedList(getMyFeed());
+        setStoryList(getMyFeed());
+
+    }, [])
+
+    return(
+        <FlatList 
+            data={feedList}
+            keyExtractor={ (item, index) => {
+                return `myfeed-${index}`
+            }}
+            showsVerticalScrollIndicator={false}
+            onRefresh={ ()=>{
+                setLoding(true);
+                setTimeout( ()=>{
+                    setFeedList(getMyFeed());
+                    setStoryList(getMyFeed());
+                    setLoding(false)
+                },2000);
+            }}
+            onEndReached={}
+        />
+    )
+}
+
+const styles = StyleSheet.create({
+    headerRightcontaienr : {
+        flexDirection : 'row'
+    },
+
+});
